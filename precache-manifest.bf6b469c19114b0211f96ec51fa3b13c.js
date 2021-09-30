bfe3a567a84796c7a555b7ef75ca227f3ff61dcd
Luis Cebamanos <l.cebamanos@udc.es>
2021-09-30 17:44:54 +0100

    Updates

---
+++ precache-manifest.bf6b469c19114b0211f96ec51fa3b13c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bfe65583dccd7b136dabf79e2e03adc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78aeeaf3b4b49610862d",
+    "url": "/static/css/main.e16032d7.chunk.css"
+  },
+  {
+    "revision": "657600160e34f78e0e69",
+    "url": "/static/js/2.3553cce0.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.3553cce0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78aeeaf3b4b49610862d",
+    "url": "/static/js/main.1a881cd0.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "7a824b4c55fece70da3cec9efe5927de",
+    "url": "/static/media/naranjito.7a824b4c.png"
+  },
+  {
+    "revision": "fd5d7f50a9d6d4e7ea28e5c2209f4547",
+    "url": "/static/media/resume.fd5d7f50.pdf"
+  }
+]);
