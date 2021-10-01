c88bc9db60e0c0d339492892cd3698045a42ef69
Luis Cebamanos <l.cebamanos@udc.es>
2021-10-01 09:41:54 +0100

    Updates

---
+++ precache-manifest.a29e651bf08748fd0ef1848ea51d8ad2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e37943cdc209100a70229833c59d8bc2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a09ade4e8bdab5823b02",
+    "url": "/static/css/main.e16032d7.chunk.css"
+  },
+  {
+    "revision": "83c811a81ea4030ba002",
+    "url": "/static/js/2.d9cc13f4.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.d9cc13f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a09ade4e8bdab5823b02",
+    "url": "/static/js/main.0055e89d.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "bd08919573e08c5662e11b62a2b3fbc8",
+    "url": "/static/media/inquisitive.bd089195.jpeg"
+  },
+  {
+    "revision": "7a824b4c55fece70da3cec9efe5927de",
+    "url": "/static/media/naranjito.7a824b4c.png"
+  },
+  {
+    "revision": "fd5d7f50a9d6d4e7ea28e5c2209f4547",
+    "url": "/static/media/resume.fd5d7f50.pdf"
+  }
+]);
